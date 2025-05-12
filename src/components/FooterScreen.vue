08a2737ed7bd89e1a9eda747b5833e5ef9564208
Code002 <kakashivncm123@gmail.com>
2025-05-12 18:18:20 +0700

    Update Footer Component

--- src/components/FooterScreen.vue
+++ src/components/FooterScreen.vue
@@ -1,3 +1,15 @@
 <template>
   <p>Tran Thanh Phuc</p>
 </template>
+
+<style scoped>
+p{
+  position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: -1rem;
+    color: var(--light);
+    z-index: 3;
+    font-size: 1.5rem;
+}
+</style>
