2a0006c96edb654767c22da88b1fb430d397d333
Code002 <kakashivncm123@gmail.com>
2025-05-12 18:19:02 +0700

    Update InteracScreen Component

--- src/components/InteracScreen.vue
+++ src/components/InteracScreen.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>InteracScreen</h1>
-  <card-flip
-    v-for="(card, index) in cards"
-    :key="index"
-    :imgUrl="getImageUrl(card, index)"
-    :soundUrl="getSoundUrl(card)"
-  />
+  <div class="screen">
+    <div :style="gridStyle" class="card-grid">
+      <card-flip
+        v-for="(card, index) in cards"
+        :key="index"
+        :imgUrl="getImageUrl(card, index)"
+        :soundUrl="getSoundUrl(card)"
+        :ref="`card-${index}`"
+        :card="{ index, value: card }"
+        @onFlip="checkRule($event)"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,6 +27,23 @@ export default {
     },
   },
 
+  data() {
+    return {
+      rules: [],
+      column: Math.sqrt(this.cards.length),
+    };
+  },
+
+  computed: {
+    gridStyle() {
+      return {
+        display: "grid",
+        "grid-template-columns": `repeat(${this.column}, 1fr)`,
+        gap: "-1rem",
+      };
+    },
+  },
+
   components: {
     CardFlip,
   },
@@ -30,7 +53,6 @@ export default {
       const extensions = ["jpg", "webp", "png"];
       for (let ext of extensions) {
         const url = `assets/brainrot/images/${card}.${ext}`;
-        console.log(url);
         try {
           const res = await fetch(url);
           if (res.ok) {
@@ -45,14 +67,70 @@ export default {
         }
         return null;
       }
-      // console.log(card, index);
     },
 
     getSoundUrl(card) {
       const url = `assets/brainrot/sounds/${card}.mp3`;
-      // console.log(window.location.href + url, card)
       return window.location.href + url;
+    },
+
+    checkRule(event) {
+      if (this.rules.length >= 2) {
+        // for (let key in this.rules)
+        // {
+        //   this.$refs[`card-${this.rules[key].index}`][0].flipBack();
+        //   console.log("Tràn", this.$refs[`card-${this.rules[key].index}`][0])
+        // }
+        // this.rules = [];
+        // return false;
+      }
+      this.rules.push(event);
+      if (
+        this.rules.length == 2 &&
+        this.rules[0].value == this.rules[1].value
+      ) {
+        this.$refs[`card-${this.rules[0].index}`][0].muteSound();
+        this.rules = [];
+        const disableElements = document.querySelectorAll(
+          ".card__inner.is-disable"
+        );
+
+        // Hoàn thành trò chơi
+        if (disableElements.length == this.cards.length - 1) {
+          setTimeout(() => {
+            this.$emit("onFinish");
+          }, 800);
+        }
+      } else if (
+        this.rules.length == 2 &&
+        this.rules[0].value != this.rules[1].value
+      ) {
+        setTimeout(() => {
+          this.$refs[`card-${this.rules[0].index}`][0].flipBack();
+          this.$refs[`card-${this.rules[1].index}`][0].flipBack();
+          this.rules = [];
+        }, 700);
+      }
     },
   },
 };
 </script>
+
+<style scopted>
+.screen {
+    width: 90%;
+    height: 100%;
+    margin: auto;
+}
+
+.screen__inner {
+  /* display: flex;
+  justify-content: center; */
+}
+.card-grid {
+  width: 100%;
+  /* max-width: 1200px; */
+  margin: auto;
+  /* padding: 16px; */
+}
+</style>
